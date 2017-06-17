1225b405b1eaca2467442c421f6d5440b25f575d
yatree <yatree@yandex.ru>
2017-06-17 21:46:34 +0300

    Worj with auth step 4

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/middleware');
+var middleware = require('./routes/middleware');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
