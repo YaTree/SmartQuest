ddd995bc10da39e9aa623c8e088e3ac7a16064e8
yatree <yatree@yandex.ru>
2017-06-17 20:45:31 +0300

    Worj with auth step 4

--- queries.js
+++ queries.js
@@ -1249,7 +1249,7 @@ function removeUser(req, res, next) {
 function checkUser(req, res, next) {
     var userName = parseInt(req.params.name);
     var userPassword = parseInt(req.params.password);
-    db.any('SELECT user_id FROM customer.users WHERE user_name = \'$1\' and user_password = \'$2\'', [userName,userPassword])
+    db.any("SELECT user_id FROM customer.users WHERE user_name = '$1' and user_password = '$2'", [userName,userPassword])
         .then(function (data) {
             res.status(200)
                 .json({
